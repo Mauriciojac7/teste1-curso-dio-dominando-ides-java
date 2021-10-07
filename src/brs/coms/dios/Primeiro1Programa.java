b04fc23103707bb55cd365074bbd275811c04d90
Mauriciojac7 <baixista.mjac7@gmail.com>
2021-10-06 22:17:54 -0300

    first commit

---
+++ src/brs/coms/dios/Primeiro1Programa.java
@@ -0,0 +1,27 @@
+package brs.coms.dios;
+
+import brs.coms.dios.models.Gatos;
+
+public class Primeiro1Programa {
+
+	public static void main(String[] args) {
+		
+		Gatos Gatos = new Gatos();
+		Livross Livross = new Livross();
+				
+		System.out.println(Gatos);
+		System.out.println(Livross);
+			
+				
+		/*int a = 2;
+		int b = 3;
+		
+		System.out.println("Hello World...." + (a+b));*/
+	}
+}
+
+class Livross {
+	private String nome;
+	private String npag;
+	
+}
---
+++ src/brs/coms/dios/models/Gatos.java
@@ -0,0 +1,83 @@
+package brs.coms.dios.models;
+
+import java.util.Objects;
+
+public class Gatos {
+
+	private String nome;
+	private String cor;
+	private Integer idade;
+	
+	
+	public Gatos() {}
+
+	public Gatos(String nome, String cor, Integer idade) {
+		super();
+		this.nome = nome;
+		this.cor = cor;
+		this.idade = idade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCor() {
+		return cor;
+	}	
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cor, idade, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Gatos other = (Gatos) obj;
+		return Objects.equals(cor, other.cor) && Objects.equals(idade, other.idade) && Objects.equals(nome, other.nome);
+	}
+
+	@Override
+	public String toString() {
+		return "Gatos [nome=" + nome + ", cor=" + cor + ", idade=" + idade + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	/*public Gatos (String nome, String cor, Integer idade) {
+		this.nome= nome;
+		this.cor= cor;
+		this.idade= idade;
+		
+	}*/
+	
+	
+	
+	
+}
